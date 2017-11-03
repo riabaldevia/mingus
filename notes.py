bc9ffccb6f885eab70b80f638260a1343cbeaf8d
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-11-02 23:10:53 -0400

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,15 @@
+from mingus.containers import Note
+
+Note("C") #default C is the 4th octave
+'C-4'
+
+Note("C", 4)
+'C-4'
+
+Note("C", 5) #5 notes 5th octave
+'C-5'
+
+n = Note()
+n.set_note("C", 5)
+n
+'C-5'
